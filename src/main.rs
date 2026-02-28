39de33006d61383d3456d34e1de065ae799a19a3
G8Ren <thomaswugogo@outlook.com>
2026-02-28 12:22:11 -0500

    initialize

---
+++ src/main.rs
@@ -0,0 +1,54 @@
+#![no_std]
+#![no_main]
+
+use core::arch::global_asm;
+use core::panic::PanicInfo;
+
+// 1. 内核第一条指令的汇编入口
+// OpenSBI 会跳转到这里，我们需要先设置栈指针 (sp)，否则 Rust 函数无法调用喵
+global_asm!(
+    ".section .text.entry",
+    ".globl _start",
+    "_start:",
+    "    la sp, boot_stack_top",     // 加载栈顶地址到 sp 寄存器
+    "    call rust_main",            // 跳转到 Rust 的初始化逻辑
+    "1:",
+    "    wfi",                       // (Wait For Interrupt) 睡眠等待
+    "    j 1b",                      // 死循环，防止内核退出
+    ".section .bss.uninit",
+    ".space 4096 * 16",              // 预留 64KB 的内存作为启动栈
+    "boot_stack_top:"                // 栈顶标签（栈在 RISC-V 中向下增长）
+);
+
+// 2. Rust 侧的内核主函数
+// 适配新版 Rust：显式声明干涉链接器符号表是 unsafe 的操作喵
+#[unsafe(no_mangle)]
+pub extern "C" fn rust_main() -> ! {
+    clear_bss();
+    
+    // TODO: 在这里我们将实现串口 (UART) 驱动，用于打印字符
+    
+    loop {}
+}
+
+// 3. 内存清理机制
+// 在裸机环境下，加载器不会帮我们清空 BSS 段，如果包含历史垃圾数据会导致严重 Bug 喵
+fn clear_bss() {
+    unsafe extern "C" {
+        fn sbss(); // 符号由 linker.ld 注入
+        fn ebss();
+    }
+    unsafe {
+        core::slice::from_raw_parts_mut(
+            sbss as *const () as usize as *mut u8,
+            ebss as *const () as usize - sbss as *const () as usize,
+        )
+        .fill(0);
+    }
+}
+
+// 4. 崩溃处理钩子
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
